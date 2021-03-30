ea3d29ccdabb7b001c2bee8624c918281ee922da
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-03-30 12:15:51 -0300

    Create Card component and add it to main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./App.css";
 
 import Home from "./pages/Home";
+import Card from "./components/Card";
 
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Card />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card: React.FC = () => {
+  return <div>outro texto qualquer</div>;
+};
+
+export default Card;
