5e1488314340be37afaadcdbdba8710e699bcf47
Leticia Silverio <leticia.silverio09@gmail.com>
2021-03-30 18:30:02 -0300

    Create the footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import "./App.css";
 
 import Home from "./pages/Home";
-// import Card from "./components/Card";
+import Card from "./components/Card";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
     <>
       <Home />
       <Hero />
+      <Card />
+      <Footer />
     </>
   );
 };
