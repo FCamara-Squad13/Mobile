ac655f5012a7018d14c71d2bc7b90770f150d72f
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-03-30 19:23:36 -0300

    Remove unused code and empty spaces

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,15 @@ import HomeCard from "./components/HomeCard";
 // import Card from "./components/Card";
 import Hero from "./components/Hero";
 
-
 const App: React.FC = () => {
   return (
     <>
       <Home />
-
+      <Hero />
       <div className="container">
         <Card />
         <HomeCard />
       </div>
-
-      <Hero />
-
     </>
   );
 };
