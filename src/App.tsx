0126190155f7a47823cf1238a07026299ace154d
Leticia Silverio <leticia.silverio09@gmail.com>
2021-03-30 15:50:35 -0300

    Create the assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 
 import Home from "./pages/Home";
 import Card from "./components/Card";
+import Hero from "./components/Hero";
 
 const App: React.FC = () => {
   return (
     <>
       <Home />
+      <Hero />
       <Card />
     </>
   );
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Hero: React.FC = () => {
+  return (
+    <div className="hero">
+      <h2 className="title">Olá, Fulano!</h2>
+      <h3 className="subtitle">Que bom te ver por aqui :)</h3>
+    </div>
+  );
+};
+
+export default Hero;
