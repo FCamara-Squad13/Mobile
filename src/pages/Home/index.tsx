3fdce12e94bfcebb9aa2e0ebf0bc92992c0b9f97
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-03-31 15:37:51 -0300

    Put CardHolder on Homepage component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import "./style.css";
 
 import Hero from "../../components/Hero";
-import Card from "../../components/Card";
-import HomeCard from "../../components/HomeCard";
+import CardHolder from "../../components/CardHolder";
 import Footer from "../../components/Footer";
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
-      <div className="container">
-        <Card />
-        <HomeCard />
-      </div>
+      <CardHolder />
       <Footer />
     </>
   );
