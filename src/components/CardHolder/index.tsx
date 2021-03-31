966cadefb91d6264f4faed0e57dfe5a39639edc3
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-03-31 15:37:26 -0300

    Change Cards inside CardHolder

--- src/components/CardHolder/index.tsx
+++ src/components/CardHolder/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-import Card from "../Card";
+import HomeCard from "../HomeCard";
 
 const CardHolder: React.FC = () => {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <HomeCard />
+      <HomeCard />
+      <HomeCard />
     </>
   );
 };
