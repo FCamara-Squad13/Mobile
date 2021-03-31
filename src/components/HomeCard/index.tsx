e4540a753d6dc048f052488a5ce1d31003f81587
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-03-31 19:42:55 -0300

    Stylish HomeCard and Card components

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -9,23 +9,26 @@ type Props = {
 const HomeCard: React.FC<Props> = ({ imagePath }) => {
   return (
     <div className="home-card">
-      <img src={imagePath} alt="canetas" className="product-image" />
+      <div className="product">
+        <img src={imagePath} alt="canetas" className="product-image" />
+        <span className="product-tag">Usado</span>
+      </div>
       <div className="product-data">
         <h3 className="product-title">Lápis de cor 24 cores</h3>
-        <p className="product-location">Curitiba | PR</p>
       </div>
       <div className="announcement-data">
+        <p className="product-location">Curitiba | PR</p>
         <p className="announcement">
-          Anunciado em <span className="annoucement-date">28/03/2021</span>
+          Anunciado em: <br />
+          28/03/2021
         </p>
-        <span className="product-tag">Usado</span>
       </div>
       <div className="delivery-conditions">
         <span className="delivery-tag">Entrego pessoalmente</span>
-        <span className="delivery-tag">Envio</span>
+        {/* <span className="delivery-tag">Envio</span> */}
       </div>
-      <button type="button" className="more-details-button">
-        Ver detalhes
+      <button type="button" className="see-details-button">
+        Detalhes
       </button>
     </div>
   );
