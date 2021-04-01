07b069c109edcd7bf3e1fde680d60b1190893245
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-01 13:53:13 -0300

    Edit section title font

--- src/components/CardHolder/index.tsx
+++ src/components/CardHolder/index.tsx
@@ -1,18 +1,33 @@
 import React from "react";
+import "./style.css";
 
 import HomeCard from "../HomeCard";
 
 import canetas from "../../assets/images/Canetas.png";
 import lapis from "../../assets/images/Lapis.jpg";
 import lancheira from "../../assets/images/Lancheira.jpg";
+import ButtonVerMais from "../ButtonVerMais";
 
 const CardHolder: React.FC = () => {
   return (
-    <>
-      <HomeCard imagePath={canetas} />
-      <HomeCard imagePath={lapis} />
-      <HomeCard imagePath={lancheira} />
-    </>
+    <section className="section">
+      <div className="container">
+        <div className="section-content">
+          <div className="section-header">
+            <h3 className="section-title">O que estão doando na sua região</h3>
+            <div className="section-nav">
+              <p> {`<= =>`} </p>
+            </div>
+          </div>
+          <div className="card-holder">
+            <HomeCard imagePath={canetas} />
+            <HomeCard imagePath={lapis} />
+            <HomeCard imagePath={lancheira} />
+          </div>
+          <ButtonVerMais />
+        </div>
+      </div>
+    </section>
   );
 };
 
