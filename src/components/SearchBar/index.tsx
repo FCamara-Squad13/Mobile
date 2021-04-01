954e6d0900716e70abac14e02178ab70f75700a0
Leticia Silverio <leticia.silverio09@gmail.com>
2021-04-01 19:31:22 -0300

    Create the structure of the component

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Search: React.FC = () => {
+  return (
+    <div className="Search">
+      <div className="container">
+        <div className="Search-content" />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
