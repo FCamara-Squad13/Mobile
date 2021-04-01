4b08591b27d65e0ce0d279f5197ff8829803e19f
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-01 18:08:58 -0300

    Renames page Precisando

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,13 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Teste from "../pages/Teste";
 import Doando from "../pages/Doando";
-import pedindoDoacoes from "../pages/pedindoDoacoes";
+import Precisando from "../pages/Precisando";
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/doacao" component={Doando} />
-    <Route path="/pedindo-doacoes" component={pedindoDoacoes} />
+    <Route path="/precisando" component={Precisando} />
     <Route path="/teste" component={Teste} />
   </Switch>
 );
