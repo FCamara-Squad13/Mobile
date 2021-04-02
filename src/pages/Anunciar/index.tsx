b88da5c9163c563af96c239f32de4bfe7f23c5d2
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-02 14:16:45 -0300

    Add Navbar component to Anunciar page

--- src/pages/Anunciar/index.tsx
+++ src/pages/Anunciar/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
 const Anunciar: React.FC = () => {
   return (
     <>
+      <Navbar />
       <h1>Página Anunciar</h1>
       <Link to="/">
         <p>Voltar</p>
