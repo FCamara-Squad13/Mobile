af3b1e4b747ca492be7563bedfcecb118f347cad
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-02 18:43:35 -0300

    Fix navbar link alignment

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,15 +11,11 @@ const Home: React.FC = () => {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
-      <Hero title="Olá, Pamela!" subtitle="Que bom te ver por aqui :)" />
-=======
       <Hero
         title="Olá, Pamela!"
         subtitle="Que bom te ver por aqui :)"
         heroPath={homeHero}
       />
->>>>>>> 7adf85b06b0cbd457054fb001ee47e57bb52254f
       <CardHolder title="O que estão doando na sua região" />
       <CardHolder title="Quem está precisando de doação na sua região" />
       <Footer />
