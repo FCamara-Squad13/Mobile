5da31ab3b620191c91b931805773a9696643d806
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-02 09:39:04 -0300

    Adjust Card style

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./style.css";
-import { ReactComponent as ImagePlaceholder } from "../../assets/image-placeholder.svg";
 
-const Card: React.FC = () => {
+type Props = {
+  imagePath: string;
+};
+
+const Card: React.FC<Props> = ({ imagePath }) => {
   return (
     <div className="card">
-      <ImagePlaceholder className="product-image" />
+      <div className="product">
+        <img src={imagePath} alt="canetas" className="product-image" />
+        <span className="product-tag">Usado</span>
+      </div>
       <div className="product-data">
         <h3 className="product-title">Lápis de cor 24 cores</h3>
         <p className="product-location">Curitiba | PR</p>
@@ -15,10 +21,7 @@ const Card: React.FC = () => {
         </p>
       </div>
       <div className="announcement-data">
-        <p className="announcement">
-          Anunciado em <span className="annoucement-date">28/03/2021</span>
-        </p>
-        <span className="product-tag">Usado</span>
+        <p className="announcement">Anunciado em 28/03/2021</p>
       </div>
       <div className="delivery-conditions">
         <h3 className="delivery-conditions__title">Condições de entrega</h3>
@@ -26,8 +29,8 @@ const Card: React.FC = () => {
         <span className="delivery-tag">Entrego pessoalmente</span>
         <span className="delivery-tag">Envio</span>
       </div>
-      <button type="button" className="more-details-button">
-        Ver detalhes
+      <button type="button" className="see-details-button">
+        Detalhes
       </button>
     </div>
   );
