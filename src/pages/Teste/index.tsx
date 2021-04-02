c00a5b6e4030d8ceb1ef24f9e05bff87f00cbf66
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-02 09:46:41 -0300

    Add styled cards to test page

--- src/pages/Teste/index.tsx
+++ src/pages/Teste/index.tsx
@@ -5,6 +5,11 @@ import CardHolder from "../../components/CardHolder";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Hero from "../../components/Hero";
+import Card from "../../components/Card";
+
+import canetas from "../../assets/images/Canetas.png";
+import lapis from "../../assets/images/Lapis.jpg";
+import lancheira from "../../assets/images/Lancheira.jpg";
 
 const Teste: React.FC = () => {
   return (
@@ -13,6 +18,9 @@ const Teste: React.FC = () => {
       <Hero title="" subtitle="" />
       <CardHolder title="O que estão doando na sua região" />
       <CardHolder title="Quem está precisando de doação na sua região" />
+      <Card imagePath={canetas} />
+      <Card imagePath={lapis} />
+      <Card imagePath={lancheira} />
       <Footer />
     </>
   );
