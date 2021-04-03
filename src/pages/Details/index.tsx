8032807863b84d01edbd51f0f7c3ef329915751a
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-03 16:25:49 -0300

    Add 'Detalhes' page and add it to Router

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Detalhes: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Detalhes</h1>
+    </div>
+  );
+};
+
+export default Detalhes;
