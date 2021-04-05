37f7685d6ddadcee24fbadc21c1d471199ee8af8
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-04-05 09:18:06 -0300

    Create things on Chat page

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import GoBack from "../../components/GoBack";
+
+import "./style.css";
+
+import alert from "../../assets/images/alert.png";
 
 const Home: React.FC = () => {
   return (
-    <>
+    <div className="container">
+      <GoBack />
       <h1>hi</h1>
-      <h1>hi</h1>
-    </>
+      <div className="chat-page-content">
+        <div className="chat-area">
+          <p className="disclaimer">
+            Nossa plataforma não solicita nenhum tipo de código ou dado
+            sensível, não forneça informações pessoais como dados de conta
+            bancária e cartão de crédito para estranhos.
+          </p>
+          <p className="disclaimer">Percebeu alguma atividade suspeita?</p>
+          <div className="complaint-wrapper">
+            <img src={alert} alt="triângulo com sinal de exclamação dentro" />
+            <p className="complaint">Fazer uma denúncia</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
